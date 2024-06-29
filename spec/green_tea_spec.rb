833c473b2b131f4c4be30fc9813842b00e3c0534
Genesio Valois <genas96@gmail.com>
2024-06-29 15:04:22 +0100

    fix: update expect name

--- spec/green_tea_spec.rb
+++ spec/green_tea_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe GreenTea do
   it 'test initial GreenTea values' do
     green_tea = GreenTea.new
     expect(green_tea.code).to eq('GR1')
-    expect(green_tea.name).to eq('Green tea')
+    expect(green_tea.name).to eq('Green Tea')
     expect(green_tea.price).to eq(3.11)
   end
 end
