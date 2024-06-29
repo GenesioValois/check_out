a2cd252357f328b0c5e88ac106d42f5401299402
Genesio Valois <genas96@gmail.com>
2024-06-29 18:26:25 +0100

    feat: Add discounted_price logic to calculate_price

--- lib/services/calculate_price.rb
+++ lib/services/calculate_price.rb
@@ -15,8 +15,31 @@ module Services
 
     def call
       basket.products.sum(0.0) do |product|
-        product.price
+        final_price(product)
       end
+    end
+
+    private
+
+    def final_price(product)
+      check_promotion(product.code) ? product.discounted_price : product.price
+    end
+
+    def check_promotion(code)
+      return promotion[code] if promotion[code]
+
+      promotion[code] =
+        case code
+        when 'GR1'
+          basket.codes == %w[GR1 GR1]
+        when 'SR1'
+          basket.codes.filter { |c| c == code }.size >= 3
+        when 'CF1'
+          basket.codes.filter { |c| c == code }.size >= 3
+        else
+          false
+        end
+      promotion[code]
     end
   end
 end
