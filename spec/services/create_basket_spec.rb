805298fd672c82d172063bf502d78e0fbe2d864c
Genesio Valois <genas96@gmail.com>
2024-06-29 19:56:30 +0100

    test: Use only correct codes

--- spec/services/create_basket_spec.rb
+++ spec/services/create_basket_spec.rb
@@ -7,4 +7,11 @@ RSpec.describe Services::CreateBasket do
     expect(basket.class).to eq(Basket)
     expect(basket.size).to eq(2)
   end
+
+  it 'Should create a basket with usable codes only' do
+    basket = Services::CreateBasket.call('SR1, SR1, EE1')
+
+    expect(basket.class).to eq(Basket)
+    expect(basket.size).to eq(2)
+  end
 end
