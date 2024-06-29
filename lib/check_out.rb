066307866b01bfe875f5a92a4ee5fb47a0595ff0
Genesio Valois <genas96@gmail.com>
2024-06-29 16:51:46 +0100

    feat: Load create_basket  services

--- lib/check_out.rb
+++ lib/check_out.rb
@@ -5,6 +5,7 @@ require_relative 'check_out/green_tea'
 require_relative 'check_out/strawberries'
 require_relative 'check_out/coffee'
 require_relative 'check_out/basket'
+require_relative 'services/create_basket'
 
 module CheckOut
   class Error < StandardError; end
