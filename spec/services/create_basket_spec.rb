55883c05a3454c80192186c3aabebafbc0d570f1
Genesio Valois <genas96@gmail.com>
2024-06-29 17:03:17 +0100

    test: Confirm if the result is a Basket

--- spec/services/create_basket_spec.rb
+++ spec/services/create_basket_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe Services::CreateBasket do
   it 'Should create a basket from a list of codes' do
     basket = Services::CreateBasket.call('SR1, SR1')
 
+    expect(basket.class).to eq(Basket)
     expect(basket.size).to eq(2)
   end
 end
