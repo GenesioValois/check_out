4753b75e430dd8e8b1446b1787af4f399fcaf32d
Genesio Valois <genas96@gmail.com>
2024-06-29 15:01:48 +0100

    feat: Add green_tea object

--- lib/check_out.rb
+++ lib/check_out.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative "check_out/version"
+require_relative "check_out/green_tea"
 
 module CheckOut
   class Error < StandardError; end
---
+++ lib/check_out/green_tea.rb
@@ -0,0 +1,2 @@
+class GreenTea
+end
