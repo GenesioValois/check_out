bd80d2f5bc7349a362ccd4a17155544036ae3b11
Genesio Valois <genas96@gmail.com>
2024-06-29 15:14:57 +0100

    feat: Add coffee object

--- lib/check_out.rb
+++ lib/check_out.rb
@@ -3,6 +3,7 @@
 require_relative 'check_out/version'
 require_relative 'check_out/green_tea'
 require_relative 'check_out/strawberries'
+require_relative 'check_out/coffee'
 
 module CheckOut
   class Error < StandardError; end
