88a142311a0d0ce06ac44adb7532c0221e17fc4b
Genesio Valois <genas96@gmail.com>
2024-06-29 15:10:39 +0100

    feat: import strawberries

--- lib/check_out.rb
+++ lib/check_out.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
-require_relative "check_out/version"
-require_relative "check_out/green_tea"
+require_relative 'check_out/version'
+require_relative 'check_out/green_tea'
+require_relative 'check_out/strawberries'
 
 module CheckOut
   class Error < StandardError; end
